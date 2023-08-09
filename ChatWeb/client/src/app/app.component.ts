2445b7be04d835f89a2d169c0d6bbdd17bd32143
Ndhhiep <hiep.ndh2376@sinhvien.hoasen.edu.vn>
2023-08-09 20:12:45 +0700

    client

---
+++ ChatWeb/client/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
+import { Observable } from 'rxjs';
+import { ChatModel } from './models/chat.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+
+  chat$!: Observable<any>;
+  messages: any[] = [];
+  roomId: string = '';
+  newMassage: string = '';
+  userName: string = '';
+
+  constructor(private chatService: ChatService) {}
+
+  joinRoom(roomId: string) {
+    if (roomId || this.userName) {
+      console.log('Already joined in: ', roomId);
+      this.chat$ = this.chatService.getMessagesByRoomId(roomId);
+      this.chat$.subscribe((message: any) => {
+        this.messages.push(message);
+      });
+    }
+    else{
+      console.log('Please enter username and room id');
+    }
+  }
+
+  sendMessage(message: string) {
+    let newMassageData: ChatModel = {
+      roomId: this.roomId,
+      msg: message,
+      date: Date.now(),
+      from: this.userName
+    };
+    this.chatService.sendMessageByRoomId(newMassageData);
+  }
+}
---
+++ ChatWeb/client/src/app/models/chat.model.ts
@@ -0,0 +1,7 @@
+
+export interface ChatModel {
+    roomId: string;
+    msg: string;
+    date: number;
+    from: string;
+}
